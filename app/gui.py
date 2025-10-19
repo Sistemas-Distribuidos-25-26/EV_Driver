0efa1514f18dac171d86819d45dd5edcc68a4003
Marcos <mgf108@alu.ua.es>
2025-10-19 12:08:07 +0200

    Added styles and charge button

--- app/gui.py
+++ app/gui.py
@@ -1,7 +1,8 @@
 import dash
-from dash import html, dcc, Output, Input
+from dash import html, dcc, Output, Input, State
 from flask import Flask
 import logging
+#from kafka_producer import make_request
 
 server = Flask(__name__)
 log = logging.getLogger('werkzeug')
@@ -9,10 +10,21 @@ log.setLevel(logging.ERROR)
 
 app = dash.Dash(__name__, assets_folder="assets", server=server)
 app.layout = html.Div([
-    html.H3("Selecciona un punto de recarga:"),
+    html.H3("Selecciona un punto de recarga"),
+    html.P("Tendrás que solicitar permiso a la central. Una vez validada tu solicitud, podrás recargar tu vehículo"),
     dcc.Input(placeholder="ID del CP", id="cp_input"),
-    html.Button("Solicitar suministro", disabled=True, id="request_button")
+    html.Button("Solicitar suministro", disabled=True, id="request_button", n_clicks=0),
+    html.Button("Recargar", disabled=True, id="charge_button", n_clicks=0)
+
 ], id="main-div")
+
+@app.callback(
+    Input("request_button", "n_clicks"),
+    State("cp_input", "value")
+)
+def request(n, value):
+    print(f"[App] Mandando solicitud de recarga en {value}...")
+    #make_request(value)
 
 @app.callback(
     Output("request_button", "disabled"),
